5247c4942b85b5d2ca1d4fb1fae63f99fd0c3047
lip-senac <lip-senac@gmail>
2025-08-01 16:53:57 -0300

    falta pouco

--- app.py
+++ app.py
@@ -6,34 +6,25 @@ import sqlite3 as sql
 app = fk.Flask(__name__)
 
 @app.get('/')
-def casa_claro():
-    # conn = sql.Connection('./curiosidades.db')
-    # id = '''
-    #     SELECT id
-    #     FROM curiosidades
-    # '''
-
-    # id = conn.execute(id)
-    # quantos_id = 0
-    # for _ in id:
-    #     quantos_id = quantos_id + 1
-
-    # id_aleatorio = random.randrange(1, quantos_id+1)
-
-    # print(id_aleatorio)
-
-    # sql_select_a_curiosidade = f'''
-    #     SELECT filme, fato, img, nome AS nome_categoria
-    #     FROM curiosidades
-    #     JOIN categorias ON id_categoria == categorias.id
-    #     WHERE curiosidades.id LIKE {id_aleatorio};
-    # '''
-    # a_curiosidade = conn.execute(sql_select_a_curiosidade)
-
+def casa_br():
     return fk.render_template('br/index.html', 
                        paragrafo = ran.paragrafo(),
                        titulo = ran.titulo(),
                        elementos = ran.curisidade_aleatoria())
+
+@app.get('/adm')
+def adm():
+    with sql.Connection('./curiosidades.db') as conn:
+        sql_select_curiosidades = '''
+        SELECT filme, fato, img, nome AS nome_categoria
+        FROM curiosidades
+        JOIN categorias ON id_categoria == categorias.id;
+    '''
+        lista_de_curiosidades = conn.execute(sql_select_curiosidades)
+    
+    adm = True
+
+    return fk.render_template('br/curiosities.html', curiosidades = lista_de_curiosidades, adm = adm)
 
 @app.get('/curiosidades')
 def cards_curiosidades():
@@ -44,12 +35,58 @@ def cards_curiosidades():
         JOIN categorias ON id_categoria == categorias.id;
     '''
         lista_de_curiosidades = conn.execute(sql_select_curiosidades)
-        
-    return fk.render_template('br/curiosities.html', curiosidades = lista_de_curiosidades)
 
-@app.get('/en')
-def home_light():
-    return fk.render_template('en/template.html')
+    adm = False
+    
+    return fk.render_template('br/curiosities.html', curiosidades = lista_de_curiosidades, adm = adm)
+
+@app.get('/buscar')
+def get_buscar():
+    return fk.render_template('br/search.html')
+
+@app.post('/buscar')
+def post_buscar():
+    filme = fk.request.form['filme']
+    
+    sql_select_filme_por_nome = f'''
+    SELECT filme, fato, img, nome AS nome_categoria
+    FROM curiosidades
+    JOIN categorias ON id_categoria == categorias.id
+    WHERE filme LIKE '%{filme}%' ORDER BY nome ASC;
+'''
+    with sql.Connection('curiosidades.db') as conn:
+        # if filme:
+        lista_de_filmes = conn.execute(sql_select_filme_por_nome)
+        # else:
+        #     lista_de_filmes = [('a', 'b', 'c', 'd')]
+
+    return fk.render_template('br/curiosities.html', curiosidades = lista_de_filmes)
+
+@app.get('/editar/<id_filme>')
+def buscar(id_filme):
+    with sql.Connection('curiosidades.db') as conn:
+        sql_select_categorias = "SELECT id, nome FROM categorias;"
+        sql_dados_curiosidade = f'''
+            SELECT filme, fato, img, nome AS nome_categoria
+            FROM curiosidades
+            JOIN categorias ON id_categoria == categorias.id;
+            WHERE id = {id_filme}
+            '''
+        lista_categorias = conn.execute(sql_select_categorias)    
+        registro_curiosidade = conn.execute(sql_dados_curiosidade)
+
+        filme, fato, img, nome_categoria = next(registro_curiosidade)
+        dados_curiosidade = {
+            "id": id,
+            "nome": filme,
+            "preco": fato,
+            "img": img,
+            "id_categoria": nome_categoria
+        }
+
+        return fk.render_template('br/edit.html',
+                                     categorias = lista_categorias,
+                                     curiosidade = dados_curiosidade)
 
 @app.get("/cadastrar")
 def get_cadastrar():
@@ -80,5 +117,9 @@ def fazer_cadastro():
         conn.execute(sql_inserir_curiosidade, (filme, curiosidade, nome_imagem, categoria))
 
     return fk.redirect("/curiosidades")
+
+@app.get('/en')
+def home_light():
+    return fk.render_template('en/template.html')
 
 app.run(host='0.0.0.0', debug=True)
